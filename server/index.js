9305fb7bec6917b88161dd857d1a8246259a4849
lyokocode <aelita.dev@gmail.com>
2023-07-11 16:54:53 +0300

    db connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from "express"
+import { sequelize } from "./database/db.js"
+import cors from "cors"
+import dotenv from "dotenv"
+
 
 const app = express()
+dotenv.config()
+app.use(cors())
 
 
-app.listen(5000, () => {
-    console.log("backend is running from port:5000")
-})
+
+async function main() {
+    try {
+        await sequelize.sync({ force: true });
+        console.log("db connection is successfull")
+        app.listen(process.env.PORT, () => console.log(`api is running on port: ${process.env.PORT}`))
+
+    } catch (error) {
+        console.log(`Unable to connect to the database ${error}`)
+    }
+}
+main()
