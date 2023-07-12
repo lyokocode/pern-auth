05b1bdc95a51eb45416a252e6aedc13c8c8f47dd
lyokocode <aelita.dev@gmail.com>
2023-07-12 16:01:53 +0300

    created error middleware

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,50 +1,50 @@
 import { User } from "../models/User.js"
 
-
 // GET ALL USER
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.findAll({});
         res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" });
+    } catch (err) {
+        next(err)
     }
 };
 
 // GET USER
-export const getUser = async (req, res) => {
-    const { id } = req.query;
-
-    try {
-        const user = await User.findByPk(id);
-        if (user) {
-            return res.json({ user });
-        } else {
-            return res.status(404).json({ error: 'Kullanıcı bulunamadı.' });
-        }
-    } catch (error) {
-        return res.status(500).json({ error: 'Sunucu hatası.' });
-    }
-}
-
-// CREATE NEW USER
-export const createUser = async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-        return res.status(201).json({ user });
-    } catch (error) {
-        return res.status(500).json({ error: 'Sunucu hatası.' });
-    }
-}
-
-// DELETE USER
-export const deleteUser = async (req, res) => {
+export const getUser = async (req, res, next) => {
     const { id } = req.query;
 
     try {
         const user = await User.findByPk(id);
         if (!user) {
-            return res.status(404).json({ error: 'Kullanıcı bulunamadı.' });
+            return res.status(404).json({ error: 'User not found!' });
+        }
+
+        return res.json({ user });
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+// CREATE NEW USER
+export const createUser = async (req, res, next) => {
+    try {
+        const user = await User.create(req.body);
+        return res.status(201).json({ user });
+    } catch (err) {
+        next(err)
+    }
+}
+
+// DELETE USER
+export const deleteUser = async (req, res, next) => {
+    const { id } = req.query;
+
+    try {
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found!' });
         }
 
         await User.destroy({
@@ -53,26 +53,23 @@ export const deleteUser = async (req, res) => {
             }
         });
 
-        return res.json({ message: 'Kullanıcı silindi.' });
-    } catch (error) {
-        return res.status(500).json({ error: 'Sunucu hatası.' });
+        return res.json({ message: 'User has been deleted' });
+    } catch (err) {
+        next(err)
     }
 }
 
-
 // UPDATE USER
-
-export const updateUser = async (req, res) => {
+export const updateUser = async (req, res, err) => {
     const { id } = req.query;
     const updatedFields = req.body;
 
     try {
         const user = await User.findByPk(id);
         if (!user) {
-            return res.status(404).json({ error: 'Kullanıcı bulunamadı.' });
+            return res.status(404).json({ error: 'User not found' });
         }
 
-        // Yalnızca belirli parametreleri güncelle
         Object.keys(updatedFields).forEach((field) => {
             if (field !== 'id') {
                 user[field] = updatedFields[field];
@@ -82,7 +79,7 @@ export const updateUser = async (req, res) => {
         await user.save();
 
         return res.json({ user });
-    } catch (error) {
-        return res.status(500).json({ error: 'Sunucu hatası.' });
+    } catch (err) {
+        next(err)
     }
 }
--- server/index.js
+++ server/index.js
@@ -14,6 +14,17 @@ app.use(express.json());
 app.use("/api/auth", authRoute)
 app.use("/api/user", userRoute)
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
 async function main() {
     try {
         await sequelize.sync();
