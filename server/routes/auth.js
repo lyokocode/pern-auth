40c9c28e0c9c2d469dc836f5ca57a9c7b479ace9
lyokocode <aelita.dev@gmail.com>
2023-07-13 21:11:23 +0300

    Fix authorization logic in verifyUser function and add check endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { login, register } from "../controllers/Auth.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
@@ -11,10 +11,14 @@ router.post("/register", register)
 router.post("/login", login)
 
 // CHECK
-
 router.get("/check", verifyToken, (req, res, next) => {
     res.send("hello user you are logged in")
 })
+
+// CHECK
+router.get("/check/:id", verifyUser, (req, res, next) => {
+    res.send("Hello user, you are logged in and you can delete only your account.");
+});
 
 
 export default router
